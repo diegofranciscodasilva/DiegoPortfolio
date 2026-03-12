b71dcc3ef2d53877a4cc64f1ee9c9b028c5ec429
diegofranciscodasilva <diegofranciscodasilva@yahoo.com.br>
2026-03-12 15:08:47 -0300

    atualizando os arquivos html, css e javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,3 +26,68 @@ const shadowHeader = () => {
     this.scrollY >= 50 ? header.classList.add("shadow_header") : header.classList.remove("shadow_header")
 }
 window.addEventListener("scroll", shadowHeader)
+
+const contactForm = document.getElementById("contact-form")
+const contactMessage = document.getElementById("contact-message")
+const sendEmail = (e) => {
+    e.preventDefault()
+
+    emailjs.sendForm("service_8yvwsoh", "template_bf8sk0p", "#contact-form", "090hj4xBN59VEhOvj")
+        .then(() => {
+            contactMessage.textContent = "Mensagem enviada com sucesso 💾"
+            setTimeout(() => {
+                contactMessage.textContent = ""
+            }, 5000)
+            contactForm.reset()
+        }, () => {
+            contactMessage.textContent = "A mensagem não foi enviada (service error) 🛠️"
+        })
+}
+contactForm.addEventListener("submit", sendEmail)
+
+const scrollUp = () => {
+    const scrollUp = document.getElementById("scroll-up")
+    this.scrollY >= 350 ? scrollUp.classList.add("show-scroll") : scrollUp.classList.remove("show-scroll")
+}
+window.addEventListener("scroll", scrollUp)
+
+const sections = document.querySelectorAll("section[id]")
+const scrollActive = () => {
+    const scrollDown = window.scrollY
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 72,//58
+            sectionId = current.getAttribute("id"),
+            sectionsClass = document.querySelector(".nav_menu a[href*=" + sectionId + "]")
+
+        if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+            sectionsClass.classList.add("active-link")
+        } else {
+            sectionsClass.classList.remove("active-link")
+        }
+    })
+}
+window.addEventListener("scroll", scrollActive)
+
+const themeButton = document.getElementById("theme-button")
+const darkTheme = "dark-theme"
+const iconTheme = "ri-sun-line"
+
+const selectedTheme = localStorage.getItem("selected-theme")
+const selectedIcon = localStorage.getItem("selected-icon")
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? "dark" : "light"
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? "ri-moon-line" : "ri-sun-line"
+
+if (selectedTheme) {
+    document.body.classList[selectedTheme === "dark" ? "add" : "remove"](darkTheme)
+    themeButton.classList[selectedIcon === "ri-moon-line" ? "add" : "remove"](iconTheme)
+}
+themeButton.addEventListener("click", () => {
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+
+    localStorage.setItem("selected-theme", getCurrentTheme())
+    localStorage.setItem("selected-icon", getCurrentIcon())
+})
